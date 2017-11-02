ae614e30e2997b108542fac4d04d76019b752689
Vijay <vshukla684@gmail.com>
2017-11-02 12:02:29 +0530

    Create phantomjs Log on different place

--- src/main/groovy/GebConfig.groovy
+++ src/main/groovy/GebConfig.groovy
@@ -6,14 +6,24 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriverService
 import org.openqa.selenium.remote.DesiredCapabilities
 
+List<String> cliArgsCap = []
+cliArgsCap.add("--web-security=no")
+cliArgsCap.add("--ssl-protocol=any")
+cliArgsCap.add("--ignore-ssl-errors=yes")
+cliArgsCap.add("--webdriver-logfile=/tmp/phantomjsdriver.log")
+cliArgsCap.add("--webdriver-loglevel=ERROR")
+
 driver = {
     DesiredCapabilities caps = new DesiredCapabilities()
     caps.setJavascriptEnabled(true);
     caps.setCapability("takesScreenshot", true)
+
     caps.setCapability(
             PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
             "/usr/local/share/phantomjs/bin/phantomjs"
     )
+    caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap)
+
     new PhantomJSDriver(caps)
 }
 //htmlunit { driver = { new HtmlUnitDriver(true) } }
@@ -35,6 +45,7 @@ environments {
                 PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
                 "/usr/local/share/phantomjs/bin/phantomjs"
         )
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap)
         WebDriver driver = new PhantomJSDriver(caps)
     }
     firefox {
