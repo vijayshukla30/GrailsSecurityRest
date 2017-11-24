f5c4797d8194809ed43325bc317a3f2fb09dadfe
Puneet <puneet@nexthoughts.com>
2017-11-24 11:49:34 +0530

    bug fix. Personal Details section was not opening in admin page due to null value in date

--- src/main/groovy/com/moniday/command/PersonalDetailCO.groovy
+++ src/main/groovy/com/moniday/command/PersonalDetailCO.groovy
@@ -29,6 +29,6 @@ class PersonalDetailCO implements Validateable {
         this.country = AppUtil.getEnumByString(dataMap?.countryOfResidence, Country)
         this.nationality = AppUtil.getEnumByString(dataMap?.nationality, Country)
         this.currency = AppUtil.getEnumByString(dataMap?.currency, Currency)
-        this.dateOfBirth = AppUtil.generateDateFromString(dataMap?.dateOfBirth?.transactionDate, dataMap?.dateOfBirth?.month, dataMap?.dateOfBirth?.year)
+        this.dateOfBirth = AppUtil.generateDateFromString(dataMap?.dateOfBirth?.date, dataMap?.dateOfBirth?.month, dataMap?.dateOfBirth?.year)
     }
 }
